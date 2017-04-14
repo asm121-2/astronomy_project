128be186a229fe205acd4d5458af70773ac53266
asm121-2 <amartin4president@gmail.com>
2017-04-14 17:54:01 -0400

    Update astron.js

--- astron.js
+++ astron.js
@@ -1,809 +1,688 @@
 
-
 /**
-
- * Created by Austin
-on 4/12/2017.
-
+ * Created by Austinon 4/12/2017.
  */
-
  
-
  
-
  
-
 /*Solar System orbit animation true time scaled
-
  
-
- Revolution of planets
-in earth days (from Wikipedia)
-
+ Revolution of planetsin earth days (from Wikipedia)
  Mercury  : ~87.5 days
-
  Venus    : ~224.7 days
-
  Earth    : ~365.2563 days
-
- + Moon : ~27.3216
-days (around earth)
-
+ + Moon : ~27.3216days (around earth)
  Mars     : ~687,days (~1,8 year)
-
  Jupiter  : ~4,331 days (~12 years)
-
  Saturn   : ~10,747 days (~30 years)
-
  Uranus   : ~30,589 days (~84 years)
-
  Neptune  : ~59,802 days (~165 years)
-
  Pluto    : ~90,580 days (~248 years)
-
  */
-
  
-
  
-
  
-
  
-
  
-
 //effect on hover
-
 $(function () {
-
-   
-$('#pluto').mouseenter(function () {
-
+   $('#pluto').mouseenter(function () {
  
-
-           
-$('.pluto').ready(function () {
-
-               
-console.log('I work');
-
-               
-$('.pluto').css('opacity', '1');
-
+           $('.pluto').ready(function () {
+               console.log('I work');
+               $('.pluto').css('opacity', '1');
  
-
  
-
-               
-console.log('shoot');
-
+               console.log('shoot');
             });
-
     });
-
  
-
 });
-
 $(function () {
-
-   
-$('#pluto').mouseleave(function () {
-
+   $('#pluto').mouseleave(function () {
  
-
-       
-$('.pluto').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.pluto').css('opacity', '.5');
-
+       $('.pluto').ready(function () {
+           console.log('I work');
+           $('.pluto').css('opacity', '.5');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
  
-
 });
-
  
-
 $(function () {
-
-   
-$('#neptune').mouseenter(function () {
-
+   $('#neptune').mouseenter(function () {
  
-
-       
-$('.neptune').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.neptune').css('opacity', '1');
-
+       $('.neptune').ready(function () {
+           console.log('I work');
+           $('.neptune').css('opacity', '1');
  
-
  
-
             console.log('shoot');
-
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#neptune').mouseleave(function () {
-
+   $('#neptune').mouseleave(function () {
  
-
-       
-$('.neptune').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.neptune').css('opacity', '.5');
-
+       $('.neptune').ready(function () {
+           console.log('I work');
+           $('.neptune').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#uranus').mouseenter(function () {
-
+   $('#uranus').mouseenter(function () {
  
-
-       
-$('.uranus').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.uranus').css('opacity', '1');
-
+       $('.uranus').ready(function () {
+           console.log('I work');
+           $('.uranus').css('opacity', '1');
  
-
  
-
             console.log('shoot');
-
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#uranus').mouseleave(function () {
-
+   $('#uranus').mouseleave(function () {
  
-
-       
-$('.uranus').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.uranus').css('opacity', '.5');
-
+       $('.uranus').ready(function () {
+           console.log('I work');
+           $('.uranus').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#saturn').mouseenter(function () {
-
+   $('#saturn').mouseenter(function () {
  
-
-       
-$('.saturn').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.saturn').css('opacity', '1');
-
+       $('.saturn').ready(function () {
+           console.log('I work');
+           $('.saturn').css('opacity', '1');
  
-
  
-
             console.log('shoot');
-
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#saturn').mouseleave(function () {
-
+   $('#saturn').mouseleave(function () {
  
-
-       
-$('.saturn').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.saturn').css('opacity', '.5');
-
+       $('.saturn').ready(function () {
+           console.log('I work');
+           $('.saturn').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#jupiter').mouseenter(function () {
-
+   $('#jupiter').mouseenter(function () {
  
-
-       
-$('.jupiter').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.jupiter').css('opacity', '1');
-
+       $('.jupiter').ready(function () {
+           console.log('I work');
+           $('.jupiter').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#jupiter').mouseleave(function () {
-
+   $('#jupiter').mouseleave(function () {
  
-
-       
-$('.jupiter').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.jupiter').css('opacity', '.5');
-
+       $('.jupiter').ready(function () {
+           console.log('I work');
+           $('.jupiter').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#belt').mouseenter(function () {
-
+   $('#belt').mouseenter(function () {
  
-
-       
-$('.asteroids_meteorids').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.asteroids_meteorids').css('opacity', '1');
-
+       $('.asteroids_meteorids').ready(function () {
+           console.log('I work');
+           $('.asteroids_meteorids').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#belt').mouseleave(function () {
-
+   $('#belt').mouseleave(function () {
  
-
-       
-$('.asteroids_meteorids').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.asteroids_meteorids').css('opacity', '.5');
-
+       $('.asteroids_meteorids').ready(function () {
+           console.log('I work');
+           $('.asteroids_meteorids').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#mars').mouseenter(function () {
-
+   $('#mars').mouseenter(function () {
  
-
-       
-$('.mars').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.mars').css('opacity', '1');
-
+       $('.mars').ready(function () {
+           console.log('I work');
+           $('.mars').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#mars').mouseleave(function () {
-
+   $('#mars').mouseleave(function () {
  
-
-       
-$('.mars').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.mars').css('opacity', '.5');
-
+       $('.mars').ready(function () {
+           console.log('I work');
+           $('.mars').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#earth').mouseenter(function () {
-
+   $('#earth').mouseenter(function () {
  
-
-       
-$('.earth').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.earth').css('opacity', '1');
-
+       $('.earth').ready(function () {
+           console.log('I work');
+           $('.earth').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#earth').mouseleave(function () {
-
+   $('#earth').mouseleave(function () {
  
-
-       
-$('.earth').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.earth').css('opacity', '.5');
-
+       $('.earth').ready(function () {
+           console.log('I work');
+           $('.earth').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
     $('#venus').mouseenter(function () {
-
  
-
-       
-$('.venus').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.venus').css('opacity', '1');
-
+       $('.venus').ready(function () {
+           console.log('I work');
+           $('.venus').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-    $('#venus').mouseleave(function
-() {
-
+    $('#venus').mouseleave(function() {
  
-
-       
-$('.venus').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.venus').css('opacity', '.5');
-
+       $('.venus').ready(function () {
+           console.log('I work');
+           $('.venus').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-   
-$('#mercury').mouseenter(function () {
-
+   $('#mercury').mouseenter(function () {
  
-
-       
-$('.mercury').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.mercury').css('opacity', '1');
-
+       $('.mercury').ready(function () {
+           console.log('I work');
+           $('.mercury').css('opacity', '1');
  
-
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 $(function () {
-
-    $('#mercury').mouseleave(function
-() {
-
+    $('#mercury').mouseleave(function() {
  
-
-       
-$('.mercury').ready(function () {
-
-           
-console.log('I work');
-
-           
-$('.mercury').css('opacity', '.5');
-
+       $('.mercury').ready(function () {
+           console.log('I work');
+           $('.mercury').css('opacity', '.5');
  
-
-           
-console.log('shoot');
-
+           console.log('shoot');
         });
-
     });
-
 });
-
  
-
 //End planet hovers
-
  
-
 //Start update on speed
-
 /*
-
 $(document).ready(function() {
-
  
-
-    var spd = 0, r =
-0;
-
+    var spd = 0, r =0;
  
-
-   
-$('#points').on('change', function () {
-
-        spd =
-+(this.value);
-
+   $('#points').on('change', function () {
+        spd =+(this.value);
     });
-
  
-
     function start() {
-
         r += spd;
-
  
-
-       
-$('.solarsystem').css('-webkit-animation-duration', '12s + r');
-
-       
-requestAnimationFrame(start);
-
+       $('.solarsystem').css('-webkit-animation-duration', '12s + r');
+       requestAnimationFrame(start);
     }
-
  
-
     start();
-
  
+/**
+ * Created by Austinon 4/12/2017.
+ */
+ 
+ 
+ 
+/*Solar System orbit animation true time scaled
+ 
+ Revolution of planetsin earth days (from Wikipedia)
+ Mercury  : ~87.5 days
+ Venus    : ~224.7 days
+ Earth    : ~365.2563 days
+ + Moon : ~27.3216days (around earth)
+ Mars     : ~687,days (~1,8 year)
+ Jupiter  : ~4,331 days (~12 years)
+ Saturn   : ~10,747 days (~30 years)
+ Uranus   : ~30,589 days (~84 years)
+ Neptune  : ~59,802 days (~165 years)
+ Pluto    : ~90,580 days (~248 years)
+ */
+ 
+ 
+ 
+ 
+ 
+//effect on hover
+$(function () {
+   $('#pluto').mouseenter(function () {
+ 
+           $('.pluto').ready(function () {
+               console.log('I work');
+               $('.pluto').css('opacity', '1');
+ 
+ 
+               console.log('shoot');
+            });
+    });
+ 
+});
+$(function () {
+   $('#pluto').mouseleave(function () {
+ 
+       $('.pluto').ready(function () {
+           console.log('I work');
+           $('.pluto').css('opacity', '.5');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+ 
+});
+ 
+$(function () {
+   $('#neptune').mouseenter(function () {
+ 
+       $('.neptune').ready(function () {
+           console.log('I work');
+           $('.neptune').css('opacity', '1');
+ 
+ 
+            console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#neptune').mouseleave(function () {
+ 
+       $('.neptune').ready(function () {
+           console.log('I work');
+           $('.neptune').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#uranus').mouseenter(function () {
+ 
+       $('.uranus').ready(function () {
+           console.log('I work');
+           $('.uranus').css('opacity', '1');
+ 
+ 
+            console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#uranus').mouseleave(function () {
+ 
+       $('.uranus').ready(function () {
+           console.log('I work');
+           $('.uranus').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#saturn').mouseenter(function () {
+ 
+       $('.saturn').ready(function () {
+           console.log('I work');
+           $('.saturn').css('opacity', '1');
+ 
+ 
+            console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#saturn').mouseleave(function () {
+ 
+       $('.saturn').ready(function () {
+           console.log('I work');
+           $('.saturn').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#jupiter').mouseenter(function () {
+ 
+       $('.jupiter').ready(function () {
+           console.log('I work');
+           $('.jupiter').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#jupiter').mouseleave(function () {
+ 
+       $('.jupiter').ready(function () {
+           console.log('I work');
+           $('.jupiter').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#belt').mouseenter(function () {
+ 
+       $('.asteroids_meteorids').ready(function () {
+           console.log('I work');
+           $('.asteroids_meteorids').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#belt').mouseleave(function () {
+ 
+       $('.asteroids_meteorids').ready(function () {
+           console.log('I work');
+           $('.asteroids_meteorids').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#mars').mouseenter(function () {
+ 
+       $('.mars').ready(function () {
+           console.log('I work');
+           $('.mars').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#mars').mouseleave(function () {
+ 
+       $('.mars').ready(function () {
+           console.log('I work');
+           $('.mars').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#earth').mouseenter(function () {
+ 
+       $('.earth').ready(function () {
+           console.log('I work');
+           $('.earth').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#earth').mouseleave(function () {
+ 
+       $('.earth').ready(function () {
+           console.log('I work');
+           $('.earth').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+    $('#venus').mouseenter(function () {
+ 
+       $('.venus').ready(function () {
+           console.log('I work');
+           $('.venus').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+    $('#venus').mouseleave(function() {
+ 
+       $('.venus').ready(function () {
+           console.log('I work');
+           $('.venus').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+   $('#mercury').mouseenter(function () {
+ 
+       $('.mercury').ready(function () {
+           console.log('I work');
+           $('.mercury').css('opacity', '1');
+ 
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+$(function () {
+    $('#mercury').mouseleave(function() {
+ 
+       $('.mercury').ready(function () {
+           console.log('I work');
+           $('.mercury').css('opacity', '.5');
+ 
+           console.log('shoot');
+        });
+    });
+});
+ 
+//End planet hovers
+ 
+//Start update on speed
+/*
+$(document).ready(function() {
+ 
+    var spd = 0, r =0;
+ 
+   $('#points').on('change', function () {
+        spd =+(this.value);
+    });
+ 
+    function start() {
+        r += spd;
+ 
+       $('.solarsystem').css('-webkit-animation-duration', '12s + r');
+       requestAnimationFrame(start);
+    }
+ 
+    start();
+ 
+})
+*/
+ 
+$(document).ready(function() {
+ 
+    var slide =document.getElementById('slide'),
+        points =document.getElementById("points");
+ 
+    slide.onchange =function () {
+       points.innerHTML = this.value;
+       console.log('UPDATE');
+    }
+ 
+},{passive:true});
+ 
+ 
+ 
+$(document).ready(function() {
+ 
+   $('#pause').on('click',function(){
+        varself=$(this);
+ 
+       if(self.text()=="Pause")    {
+           $(this).text('Play');
+           console.log("I Work play");
+ 
+           $(document).ready(function() {
+               $('orbit').stop(true, true);
+               console.log("I  pauseplanets");
+ 
+            })
+ 
+        }
+        else {
+           $(this).text('Pause');
+           console.log("I Work pause");
+ 
+ 
+ 
+ 
+        }
+    })
+ 
+});
 
 })
-
 */
-
  
-
 $(document).ready(function() {
-
  
-
-    var slide =
-document.getElementById('slide'),
-
-        points =
-document.getElementById("points");
-
+    var slide =document.getElementById('slide'),
+        points =document.getElementById("points");
  
-
-    slide.onchange =
-function () {
-
-       
-points.innerHTML = this.value;
-
-       
-console.log('UPDATE');
-
+    slide.onchange =function () {
+       points.innerHTML = this.value;
+       console.log('UPDATE');
     }
-
  
-
 },{passive:true});
-
  
-
  
-
  
-
 $(document).ready(function() {
-
  
-
-   
-$('#pause').on('click',function(){
-
-        var
-self=$(this);
-
+   $('#pause').on('click',function(){
+        varself=$(this);
  
-
-       
-if(self.text()=="Pause")    
-{
-
-           
-$(this).text('Play');
-
-           
-console.log("I Work play");
-
+       if(self.text()=="Pause")    {
+           $(this).text('Play');
+           console.log("I Work play");
  
-
-           
-$(document).ready(function() {
-
-               
-$('orbit').stop(true, true);
-
-               
-console.log("I  pause
-planets");
-
+           $(document).ready(function() {
+               $('orbit').stop(true, true);
+               console.log("I  pauseplanets");
  
-
             })
-
  
-
         }
-
         else {
-
-           
-$(this).text('Pause');
-
-           
-console.log("I Work pause");
-
+           $(this).text('Pause');
+           console.log("I Work pause");
  
-
  
-
  
-
  
-
         }
-
     })
-
  
-
 });
-
